8d801aa8d179be1ae97991d8d00f07051afef036
N0mena <alida.rajaonarivelo@gmail.com>
2026-08-03 00:45:44 +0300

    feat: add Movie service class

--- src/main/java/hei/school/nmn/repository/MovieRepository.java
+++ src/main/java/hei/school/nmn/repository/MovieRepository.java
@@ -1,11 +1,9 @@
 package hei.school.nmn.repository;
 
 import hei.school.nmn.entity.model.JMovie;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface MovieRepository extends JpaRepository<JMovie, UUID> {
-}
+public interface MovieRepository extends JpaRepository<JMovie, UUID> {}
