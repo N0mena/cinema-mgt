3482803909441570ddee43389b89d6be00cd2077
N0mena <alida.rajaonarivelo@gmail.com>
2026-08-03 10:56:24 +0300

    feat: projection endpoints added

--- src/main/java/hei/school/nmn/endpoint/rest/controller/controller/ProjectionController.java
+++ src/main/java/hei/school/nmn/endpoint/rest/controller/controller/ProjectionController.java
@@ -1,3 +1,23 @@
 package hei.school.nmn.endpoint.rest.controller.controller;
 
-public class ProjectionController {}
+import hei.school.nmn.entity.Projection;
+import hei.school.nmn.service.ProjectionService;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+public class ProjectionController {
+  private final ProjectionService projectionService;
+
+  @GetMapping("/projections")
+  public List<Projection> getProjection() {
+    return projectionService.getAll();
+  }
+
+  @PutMapping("/projection")
+  public Projection create(@RequestBody Projection projection) {
+    return projectionService.create(projection);
+  }
+}
