f5ae29618c04e4370550c3230163837b6b3ca7fd
N0mena <alida.rajaonarivelo@gmail.com>
2026-08-06 12:26:11 +0300

    feat: /post/api/projection added and put/api/projections/id added

--- src/main/java/hei/school/nmn/endpoint/rest/controller/controller/ProjectionController.java
+++ src/main/java/hei/school/nmn/endpoint/rest/controller/controller/ProjectionController.java
@@ -3,6 +3,7 @@ package hei.school.nmn.endpoint.rest.controller.controller;
 import hei.school.nmn.entity.Projection;
 import hei.school.nmn.service.ProjectionService;
 import java.util.List;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -19,8 +20,14 @@ public class ProjectionController {
   }
 
   @PreAuthorize("hasRole('MANAGER')")
-  @PutMapping("/projection")
+  @PostMapping("/projection")
   public Projection create(@RequestBody Projection projection) {
     return projectionService.create(projection);
+  }
+
+  @PreAuthorize("hasRole('MANAGER')")
+  @PutMapping("/projections/{id}")
+  public Projection update(@PathVariable UUID id, @RequestBody Projection projection) {
+    return projectionService.update(id, projection);
   }
 }
